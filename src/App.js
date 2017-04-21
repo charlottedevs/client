877727573527d7ebc0a50f137441408ce5d0ee9e
hamholla <hamhollett@gmail.com>
2017-04-21 10:25:41 -0400

    add bootstrap styles

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.sass';
 import CreditsContainer from './components/CreditsContainer';
 import AccountContainer from './components/AccountContainer';
 
 const App = () => (
   <div className="App">
-    <div className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
+    <div className="App-header jumbotron">
       <h2><code>Steps for Code Cred</code></h2>
     </div>
     <CreditsContainer />
