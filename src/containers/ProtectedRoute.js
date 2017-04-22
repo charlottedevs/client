088089f67077f67551758c163cb6a91d43f3df4b
John Mason <john@johnmason.io>
2017-04-22 12:05:29 -0400

    Add React Router
    * Forces users to login page if not authenticated
    * Renders a 404 page if missing route
    * Adds a ProtectedRoute component for protected resources

---
+++ src/containers/ProtectedRoute.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import {
+  Route,
+  Redirect,
+} from 'react-router-dom';
+
+// eslint-disable-next-line
+const ProtectedRoute = ({ ...props, component: Component }) => {
+  if (props.isAuthenticated) {
+    return (<Route {...props} />);
+  }
+
+  return (<Redirect to="/login" />);
+};
+
+
+ProtectedRoute.propTypes = {
+  isAuthenticated: React.PropTypes.bool.isRequired,
+};
+
+function mapStateToProps(state) {
+  return {
+    isAuthenticated: state.account.isAuthenticated,
+  };
+}
+
+
+export default connect(mapStateToProps)(ProtectedRoute);
