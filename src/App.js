2c8ea2c91ed0d1bfee31b26c26a58e26bda5b0c0
John Mason <john@johnmason.io>
2017-04-27 19:35:43 -0400

    Make layout container

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import './App.sass';
-import HeaderContainer from './containers/HeaderContainer';
-import FooterContainer from './containers/FooterContainer';
+import Layout from './Layout';
 import Routes from './routes';
 
 const App = () => (
   <div className="App">
-    <HeaderContainer />
-    <Routes />
-    <FooterContainer />
+    <Layout>
+      <Routes />
+    </Layout>
   </div>
 );
 
