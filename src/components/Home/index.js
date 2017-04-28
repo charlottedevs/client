3e5533d3338be0cf20d04811d3c036e322851721
John Mason <john@johnmason.io>
2017-04-27 20:41:15 -0400

    Move challenges under protected route

---
+++ src/components/Home/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Layout from '../../Layout';
+
+const Home = () => (
+  <Layout>
+    <div className={'home'}>
+      <h1>
+        This is home
+      </h1>
+    </div>
+  </Layout>
+);
+
+export default Home;
--- src/routes.js
+++ src/routes.js
@@ -4,16 +4,18 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import ProtectedRoute from './containers/ProtectedRoute';
 import ChallengesContainer from './containers/ChallengesContainer';
 import Account from './components/Account';
+import Home from './components/Home';
 import Login from './components/Login';
 import NotFound from './components/NotFound';
 
 const Routes = () => (
   <BrowserRouter>
     <Switch>
-      <Route exact path="/" component={ChallengesContainer} />
+      <Route exact path="/" component={Home} />
       <Route path="/login" component={Login} />
 
       <ProtectedRoute path="/account" component={Account} />
+      <ProtectedRoute path="/challenges" component={ChallengesContainer} />
 
       <Route component={NotFound} />
     </Switch>
