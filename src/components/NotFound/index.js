37161937ab687967127ebdb0e74bdded89a28e1f
Joshua Lawson <jacklegjosh@gmail.com>
2017-05-20 19:23:14 -0400

    changed message and styling

--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -2,9 +2,12 @@ import React from 'react';
 
 const NotFound = () => (
   <div className={'NotFound'}>
-    <h1>
-      That is a 404.
-    </h1>
+    <h1 className={'Line'}>This is a 404.</h1>
+    <p>(or is it?)</p>
+    <h1 className={'Line'}>But seriously, it is.</h1>
+    <p>Or is it..?</p>
+    <h1 className={'Line2'}>Really, though, there&#39;s nothing here.</h1>
+    <div className={'Space'} />
   </div>
 );
 
