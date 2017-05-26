6b687aa92abe1b53933a4194836c677a9c0d5e08
KingPra <kingi.pra@gmail.com>
2017-05-26 18:35:35 -0400

    fixed leaderboard alignment issue

--- src/components/Standing/index.js
+++ src/components/Standing/index.js
@@ -3,13 +3,20 @@ import { Link } from 'react-router-dom';
 
 const Standing = ({ standing, rank }) => (
   <div className="list-group-item">
-    <p className="rank">{rank}</p>
-    <Link to={`/users/${standing.id}`}>
-      <img src={standing.picture} alt="avatar" className="avatar" />
-    </Link>
-    <p>{standing.first_name} &nbsp</p>
-    <p>{standing.last_name}</p>
-    <p className="credits">{standing.credibility}</p>
+    <div className="container">
+      <div className="row">
+        <p className="rank col-sm-1">{rank}</p>
+        <div className="col-sm-2">
+          <Link to={`/users/${standing.id}`}>
+            <img src={standing.picture} alt="avatar" className="avatar" />
+          </Link>
+        </div>
+        <div className="col-sm-6">
+          <p>{standing.first_name} {'\u00A0'} {standing.last_name}</p>
+        </div>
+        <p className="credits col-sm-1">{standing.credibility}</p>
+      </div>
+    </div>
   </div>
 
   );
