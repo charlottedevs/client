dda9d4ba1ff133f76e06348c4a90934cf5d85d5a
Joshua Lawson <jacklegjosh@gmail.com>
2017-06-04 13:51:49 -0400

    simplified text, centered horiz and vert, optimized for smaller screens

--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -2,11 +2,8 @@ import React from 'react';
 
 const NotFound = () => (
   <div className={'NotFound'}>
-    <h1 className={'Line'}>This is a 404.</h1>
-    <p>(or is it?)</p>
-    <h1 className={'Line'}>But seriously, it is.</h1>
-    <p>Or is it..?</p>
-    <h1 className={'Line2'}>Really, though, there&#39;s nothing here.</h1>
+    <h1 className={'Line'}>You look lost, friend.</h1>
+    <p className={'Line'}>Don&#39;t worry; it happens to all of us.</p>
   </div>
 );
 
